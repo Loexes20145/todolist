97cc4522a92c30459d12b91a3569f1b93053c249
loexes20145 <emaklov8145@gmail.com>
2024-09-26 11:45:55 +0000

    modified: script.js

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ function addElement () {
     const form = document.querySelector('form')
     const input = document.querySelector('.form-control')
     const ul = document.querySelector('ul')
+    
+    form.addEventListener('submit', (event) => {
+        event.preventDefault()
+
+    })
 }
 
 addElement()
